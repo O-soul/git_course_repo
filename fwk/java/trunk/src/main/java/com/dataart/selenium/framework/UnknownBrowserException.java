63cda4b8f09151a6ed592f88eed8168ce25ca9e9
O-soul <ignatev-o@mail.ua>
2016-05-05 11:25:56 +0300

    clean all

--- fwk/java/trunk/src/main/java/com/dataart/selenium/framework/UnknownBrowserException.java
+++
@@ -1,7 +0,0 @@
-package com.dataart.selenium.framework;
-
-public class UnknownBrowserException extends RuntimeException {
-    public UnknownBrowserException(String message) {
-        super(message);
-    }
-}
