527d5d82b8fd6b962caa3518ce1ef2444fa928b6
O-soul <ignatev-o@mail.ua>
2016-05-05 11:36:24 +0300

    clean all

--- fwk/java/trunk/src/main/java/com/dataart/selenium/framework/UnknownPropertyException.java
+++
@@ -1,7 +0,0 @@
-package com.dataart.selenium.framework;
-
-public class UnknownPropertyException extends RuntimeException {
-    public UnknownPropertyException(String message) {
-        super(message);
-    }
-}
